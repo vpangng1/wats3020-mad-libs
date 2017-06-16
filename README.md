b8a1c0b3124a2eeb80799eea79c85a62c045ec86
Shawn Rider <shawn@shawnrider.com>
2017-06-16 11:59:36 -0700

    Initial assignment build.
    Change-Id: I1bf671e2975dbd95edb134bcbc5ee4f21d3feaea

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# wats3020-mad-libs
-A mad libs style game used to get familiar with basic JavaScript syntax.
+# WATS 3020: Mad Libs
+
+In order to begin getting familiar with the process of writing JavaScript and
+the syntax of the language, this exercise has been designed to let you play
+with some simple commands.
+
+You will be declaring a bunch of variables and using the `prompt()` command to
+gather information from the user. The information you gather will then be
+inserted into a text template to generate a story. This project borrows the
+concept from the ["Mad Libs"](https://en.wikipedia.org/wiki/Mad_Libs) style of
+games, which is meant to be humorous because the stories often come out all
+crazy.
+
+Refer to the comments in `js/main.js` for information about everything that is
+required and how to complete all of the requirements listed below.
+
+## Project Resources
+
+It might be helpful to review some additional resources as you work through
+this project:
+
+* [window.prompt() documentation](https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt)
+* [Arithmetic Operators in JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators)
+* [JavaScript Basics](https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics)
+
+This project was also created to go along with [A Practical Introduction to JavaScript](https://www.gitbook.com/book/shawnr/practical-introduction-to-javascript/details)
+which can be accessed for free.
+
+## Basic Requirements
+
+In order to successfully complete this exercise, you must complete the following
+requirements:
+
+* Write all of the variable declarations listed in `js/main.js`
+* Properly use the `prompt()` command and write sensible text for the user to read.
+* Use at least two different arithmetic operators to generate the section numbers.
+* Update all of the comments in `js/main.js` so that they make sense to a developer coming to your code for the first time.
+* Push your source code to your `master` branch.
+* Deploy your site by creating a `gh-pages` branch and pushing your code to that branch, too.
+
+## Stretch Goals
+
+If you want to push this project further, you can try some of the following:
+
+* Enhance the styling of the page.
+* Convert the use of `prompt()` into using a proper webform (this will require you to use many more of the `document` and element manipulation methods).
+* Change the markup surrounding the output poem to facilitate more interesting stuff.
+* See how far you can push things with making up new words based on what the user submitted.
+* Use conditionals to alter some presentation or content based on the information the user has submitted.
---
+++ js/main.js
@@ -0,0 +1,118 @@
+// WATS 3020 Mad Libs Assignment
+//
+// For this assignment you must fill in the code below so that it functions
+// to populate the text of the "Mad Libs" style story in the HTML.
+//
+// In order to achieve this goal, you will need to write prompt() statements
+// to collect input from the user. The input you collect will be used to
+// generate the story.
+//
+//
+// TODO: Write a prompt statement like the one below for each of the
+// variables listed.
+
+let honorific = prompt('Please enter an honorific title (e.g. Mister or Colonel).');
+
+// Using the same prompt() command, populate the following variables:
+//
+// TODO: A variable named "authorName" for authorial attribution.
+let authorName = prompt('What is your full name?');
+
+// TODO: A variable named "adjective1" for a word used to describe something.
+
+// TODO: A variable named "vehicle" for a mode of transation (e.g. plane, boat, etc.).
+
+// TODO: A variable named "vehiclePart" for a part of the vehicle you just named.
+
+// TODO: A variable named "parking" for a location where you park the vehicle you just named.
+
+// TODO: A variable named "precious" for a precious object.
+
+// TODO: A variable named "noiseMaker" for something that makes a sound.
+
+// TODO: A variable named "crowdBehavior" for something that people do together as a group.
+
+// TODO: A variable named "adjective2" for a word used to describe something.
+
+// TODO: A variable named "adjective3" for a word used to describe something.
+
+// TODO: A variable named "color" for the name of a color.
+
+// TODO: A variable named "familyMember" for the name of a family relationship (e.g. sister, father, etc.).
+
+// TODO: A variable named "organ" for the name of a body organ (e.g. heart, liver, spleen, etc.).
+
+// TODO: A variable named "bodyPart1" for the name of a body part.
+
+// TODO: A variable named "bodyPart2" for the name of a body part.
+
+// TODO: A variable named "bodyPart3" for the name of a body part.
+
+// TODO: A variable named "thing" for an object.
+
+// TODO: A variable named "thing2" for an object you might give somebody you love.
+
+// TODO: A variable named "thing3" for an object you might give somebody you like.
+
+// TODO: A variable named "adjective4" for a word used to describe the vehicle.
+
+// TODO: A variable named "adjective5" for a word used to describe the vehicle.
+
+
+////////////////////////////////////////////////////////////////////////
+// Section Numbers
+//
+// The poem we are using has section numbers. Let's allow the user to
+// generate their own.
+//
+// TODO: To begin with, prompt the user to collect their favorite number that is not zero.
+// Call this variable "userNumber".
+
+// TODO: generate TWO more numbers called "number2" and "number 3".
+// Use any operator you would like to add, multiply, divide, or otherwise
+// change the number that the user submitted.
+
+
+// DO NOT EDIT BELOW THIS LINE /////////////////////////////////////////
+//
+// The code below his line handles variable replacement into the HTML file.
+// Please do not edit this code unless you are attempting a stretch goal.
+
+let titleText = `O ${honorific}! My ${honorific}! by <small>${authorName}</small>`;
+let titleHeading = document.querySelector("#madlib-title");
+titleHeading.innerHTML = titleText;
+
+let storyText = `
+
+    ${userNumber}
+    O ${honorific}! my ${honorific}! our ${adjective1} trip is done;
+    The ${vehicle} has weather'd every rack, the ${precious} we sought is won;
+    The ${parking} is near, the ${noiseMaker} I hear, the people all ${crowdBehavior},
+    While follow ${bodyPart1} the steady keel, the vessel ${adjective4} and ${adjective5}:
+    But O ${organ}! ${organ}! ${organ}!
+    O the bleeding drops of ${color},
+    Where on the ${vehiclePart} my ${honorific} lies,
+    Fallen ${adjective2} and ${adjective3}.
+
+    ${number2}
+    O ${honorific}! my ${honorific}! rise up and hear the ${noiseMaker};
+    Rise up-for you the ${thing} is flung-for you the bugle trills;
+    For you ${thing2} and ribbon'd ${thing3}-for you the shores a-crowding;
+    For you they ${crowdBehavior}, the swaying mass, their eager faces turning;
+    Here ${honorific}! dear ${familyMember}!
+    This ${bodyPart2} beneath your ${bodyPart3};
+    It is some dream that on the ${vehiclePart},
+    You've fallen ${adjective2} and ${adjective3}.
+
+    ${number3}
+    My ${honorific} does not answer, his lips are pale and still;
+    My ${familyMember} does not feel my arm, he has no pulse nor will;
+    The ${vehicle} is anchor'd safe and sound, its voyage closed and done;
+    From fearful trip, the victor ${vehicle}, comes in with ${precious} won;
+    Exult, O shores, and ring, O ${noiseMaker}!
+    But I, with mournful tread,
+    Walk the ${vehiclePart} my ${honorific} lies,
+    Fallen ${adjective2} and ${adjective3}.
+`;
+let storyParagraph = document.querySelector("#madlib-text");
+storyParagraph.innerHTML = storyText;
